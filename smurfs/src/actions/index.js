14747848f274d64c1c41626c452fe35eedf0db53
AceIsHuman <menoman1995@yahoo.com>
2019-09-28 13:45:20 -0500

    create deleteSmurf action

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -18,4 +18,12 @@ export const addSmurf = smurf => dispatch => {
     .then(res => {
       dispatch({ type: "SMURFS_LOADING_SUCCESS", payload: res.data });
     })
+  }
+  
+  export const deleteSmurf = id => dispatch => {
+    axios
+    .delete(`http://localhost:3333/smurfs/${id}`)
+    .then(res => {
+      dispatch({ type: "SMURFS_LOADING_SUCCESS", payload: res.data });
+    })
 }
