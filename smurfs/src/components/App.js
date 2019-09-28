7e77f299791861a3248f7a117ae730e6a77d06ff
AceIsHuman <menoman1995@yahoo.com>
2019-09-28 13:14:21 -0500

    render form to App

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
 import SmurfList from './SmurfList';
+import NewSmurfForm from "./NewSmurfForm";
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
+        <NewSmurfForm />
         <SmurfList />
       </div>
     );
